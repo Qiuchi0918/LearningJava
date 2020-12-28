5d0c499d60a07ceacf7d495f0a3414662699c012
Qiuchi0918 <chenqiuchi@outlook.com>
2020-12-28 23:00:25 +0800

    为啥编辑距离只从末尾操作就是对的

--- src/qiuchi/chen/leetcode/BestInterest.java
+++ src/qiuchi/chen/leetcode/BestInterest.java
@@ -1,6 +1,7 @@
 package qiuchi.chen.leetcode;
 
 import org.apache.derby.shared.common.util.ArrayUtil;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -50,7 +51,7 @@ class Solution {
         return bestProfit;
     }
 
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int @NotNull [] nums, int target) {
         int[] result = null;
         HashMap<Integer, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
@@ -155,8 +156,34 @@ class Solution {
         return ans;
     }
 
+    public int minDistance(String word1, String word2) {
+        if (word1.length() == 0) return word2.length();
+        if (word2.length() == 0) return word1.length();
+
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+
+        for (int i = 0; i < word1.length() + 1; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j < word2.length() + 1; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i < word1.length() + 1; i++) {
+            for (int j = 1; j < word2.length() + 1; j++) {
+                int left = dp[i - 1][j] + 1;
+                int down = dp[i][j - 1] + 1;
+                int left_down = dp[i - 1][j - 1];
+                if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
+                    left_down += 1;
+                }
+                dp[i][j] = Math.min(left, Math.min(down, left_down));
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.longestPalindrome("cbbd"));
+        solution.minDistance("horse", "ros");
     }
 }
